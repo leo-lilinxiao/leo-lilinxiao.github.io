a1a533022a3f35e48ec35a4141e9679ff5ef9365
lilinxiao <llinxiao520@gmail.com>
2026-05-27 02:19:03 +0800

    Refine publications and footer layout

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -266,11 +266,11 @@ let transTheme = () => {
 };
 
 // Determine the expected state of the theme toggle, which can be "dark", "light", or
-// "system". Default is "dark".
+// "system". Default follows the user's system preference.
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
   if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "dark";
+    themeSetting = "system";
   }
   return themeSetting;
 };
